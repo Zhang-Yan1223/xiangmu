c119e56d4d62b507bd70f9eecdd3d99d33a6f4b5
Zhang-Yan1223 <1123404992@qq.com>
2020-01-04 08:53:22 +0800

    customer

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: '家政管理平台',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: true
+}
